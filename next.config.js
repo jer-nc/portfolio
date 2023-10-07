bba7a0ffe968284dc1bc679a176b46f766a040df
rej-nc <rejdev24@gmail.com>
2023-10-07 12:29:52 +0200

    upgrade: next js 13 v13.5.4

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com', 'pbs.twimg.com','ph-files.imgix.net'], 
-        unoptimized: true,
+        // unoptimized: true,
     },
     env: {
         NEXT_PUBLIC_LAMBDA_URL: process.env.NEXT_PUBLIC_LAMBDA_URL,
